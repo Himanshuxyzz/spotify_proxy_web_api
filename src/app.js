946fd90bfcb37f12f059a38a3b36be72964a5e40
Himanshuxyzz <himanshuxyzz@gmail.com>
2023-06-28 16:21:34 +0530

    v1_build fixing:cors-issue-fixing...

--- src/app.js
+++ src/app.js
@@ -2,33 +2,18 @@
 
 require("dotenv").config();
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 const api = require("./api");
 const helmet = require("helmet");
 const app = express();
-// app.use(
-//   cors({
-//     origin: [process.env.DEPLOYED_URL, process.env.ORIGIN_URL],
-//     methods: ["GET"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//     maxAge: 600,
-//   })
-// );
-
-app.use((req, res, next) => {
-  const allowedOrigins = [
-    `${process.env.DEPLOYED_URL}`,
-    `${process.env.ORIGIN_URL}`,
-  ];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-  res.header("Access-Control-Allow-Methods", "GET");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("cache-control", "max-age=600");
-  return next();
-});
+app.use(
+  cors({
+    origin: `${process.env.DEPLOYED_URL}`,
+    methods: ["GET"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    maxAge: 600,
+  })
+);
 
 app.use(helmet());
 app.use(express.json());
